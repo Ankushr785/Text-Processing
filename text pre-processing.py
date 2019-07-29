260f4234c3da77af4fc520dc2c894e4df19f4c71
Ila Maheshwari <2014pietcsila@poornima.org>
2019-07-29 18:03:27 +0530

    Update text pre-processing.py
    added some more coding to do preprocessing for text processing.

--- text pre-processing.py
+++ text pre-processing.py
@@ -62,3 +62,53 @@ def _lookup_words(input_text):
     return new_text
 
 _lookup_words("RT this is a retweeted tweet by Shivam Bansal")
+
+
+#  Convert text to lowercase
+input_str = ”The 5 biggest countries by population in 2017 are China, India, United States, Indonesia, and Brazil.”
+input_str = input_str.lower()
+print(input_str)
+
+# Numbers removing
+import re
+input_str = ’Box A contains 3 red and 5 white balls, while Box B contains 4 red and 2 blue balls.’
+result = re.sub(r’\d+’, ‘’, input_str)
+print(result)
+
+# Remove stop words
+input_str = “NLTK is a leading platform for building Python programs to work with human language data.”
+stop_words = set(stopwords.words(‘english’))
+from nltk.tokenize import word_tokenize
+tokens = word_tokenize(input_str)
+result = [i for i in tokens if not i in stop_words]
+print (result)
+# A scikit-learn tool also provides a stop words list:
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+# It’s also possible to use spaCy, a free open-source library:
+from spacy.lang.en.stop_words import STOP_WORDS
+
+# Stemming using NLTK
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+stemmer= PorterStemmer()
+input_str=”There are several types of stemming algorithms.”
+input_str=word_tokenize(input_str)
+for word in input_str:
+    print(stemmer.stem(word))
+    
+# Part-of-speech tagging using TextBlob
+input_str=”Parts of speech examples: an article, to write, interesting, easily, and, of”
+from textblob import TextBlob
+result = TextBlob(input_str)
+print(result.tags)
+
+# Chunking using NLTK
+input_str=”A black television and a white stove were bought for the new apartment of John.”
+from textblob import TextBlob
+result = TextBlob(input_str)
+print(result.tags)
+
+# Named-entity recognition using NLTK
+from nltk import word_tokenize, pos_tag, ne_chunk
+input_str = “Bill works for Apple so he went to Boston for a conference.”
+print ne_chunk(pos_tag(word_tokenize(input_str)))
